45d9f3ab4c4087af47d22fd46151e77e8a5930c2
TSZCodes <rifqiirfanindrawan@gmail.com>
2025-11-14 00:12:07 +0700

    tempfeat: speech bubbles
    scuffed ahh textboxes

--- scripts/cat.gd
+++ scripts/cat.gd
@@ -5,6 +5,14 @@ extends Node2D
 
 var sprite: Sprite2D
 
+# Placeholder lines
+const lines : Array[String] = [
+	"I wanna kms",
+	"Pls send help",
+	"Dan if you're reading this hi",
+	"Well I'll be damned"
+]
+
 func _ready() -> void:
 	sprite = find_child("Sprite2D")
 
@@ -23,6 +31,9 @@ func _on_static_body_2d_input_event(_viewport: Node, event: InputEvent, _shape_i
 		var mouse_event: InputEventMouseButton = event
 		if mouse_event.pressed and mouse_event.button_index == MOUSE_BUTTON_LEFT:
 			Events.emit_signal("cat_mouse_click", self.position, self)
+		
+		DialogueManager.start_dialogue(global_position, lines)
 
 	elif event is InputEventMouse:
 		Events.emit_signal("cat_mouse_enter", self.position, self)
+	
--- scripts/events.gd
+++ scripts/events.gd
@@ -2,3 +2,4 @@ extends Node
 
 signal cat_mouse_click(pos: Vector2, cat: Cat)
 signal cat_mouse_enter(pos: Vector2, cat: Cat)
+signal finished_displaying()
