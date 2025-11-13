5740633b1a33b652b9ec6fee857bc4ff78c67278
SByte <stormybytes@gmail.com>
2025-11-13 18:47:49 +0700

    style: comments explaining procedural level generator

--- scripts/main.gd
+++ scripts/main.gd
@@ -44,8 +44,6 @@ func _draw() -> void:
 		draw_line(path[point_i - 1], path[point_i], Color.GREEN, 5.0)
 
 func cat_mouse_click(pos: Vector2, cat: Cat) -> void:
-	# When beginning to press and mouse is hovering above a cat,
-	# clear the existing path and append the first point in the line path
 	if not dragging:
 		start_path(pos, cat)
 
@@ -89,43 +87,69 @@ func append_path(pos: Vector2) -> void:
 	queue_redraw()
 
 func gen_cats_rect() -> void:
+	# Align the grid to the center of the screen
 	var grid_shift := DisplayServer.window_get_size() / 2
+
+	# Half of the grid size (division by 2 via bitshift)
+	# Generate min_half onion layers at most
 	var x_half := grid_size.x >> 1
 	var y_half := grid_size.y >> 1
 	var min_half := mini(x_half, y_half)
+
 	var cells_taken = []
 	for i in range(min_half):
+		# Width and height of the i-th onion layer
 		var width := (x_half - min_half + 1 + i) * 2
 		var height := (y_half - min_half + 1 + i) * 2
 		var area := 2 * (width + height - 2)
 		var pairs := area >> 1
+
 		cells_taken.clear()
 		cells_taken.resize(area)
 		cells_taken.fill(false)
+
 		for p in range(pairs):
 			var type = randi_range(1, 4)
 			for _i in range(2):
+				# Pos vec aligned with origin (0,0) at the top left corner of the i-th onion layer
+				# And (width - 1, height - 1) at the bottom right corner of the i-th onion layer
 				var pos: Vector2i
 				while true:
+					# Keep randomly selecting till a cell that is not taken yet is found
+					# Probably inefficient but meh :p
 					var ridx = randi_range(0, area - 1)
-					if not cells_taken[ridx]:
-						if ridx < width:
-							pos = Vector2i(ridx, 0)
-						elif ridx < width + height - 1:
-							pos = Vector2i(width - 1, ridx - width + 1)
-						elif ridx < 2 * width + height - 2:
-							pos = Vector2i((width - 1) - (ridx - (width + height - 1)) - 1, height - 1)
-						else:
-							pos = Vector2i(0, (height - 1) - (ridx - (2 * width + height - 2)) - 1)
-						cells_taken[ridx] = true
-						break
+					if cells_taken[ridx]:
+						continue
 
+					# Generates i-th onion layer with indicies mapped to
+					# top edge with corners,
+					# bottom edge with corners,
+					# left edge without corners,
+					# right edge without corners
+					# in that order
+					if ridx < width:
+						pos = Vector2i(ridx, 0)
+					elif ridx < 2 * width:
+						pos = Vector2i(ridx - width, height - 1)
+					elif ridx < 2 * width + height - 2:
+						pos = Vector2i(0, ridx - (2 * width) + 1)
+					else:
+						pos = Vector2i(width - 1, ridx - (2 * width + height - 2) + 1)
+
+					cells_taken[ridx] = true
+					break
+
+				# Instantiate cat
+				# Transform pos vec back into screen space
 				var instance: Cat = cat_scene.instantiate()
 				var pos_shift = pos - Vector2i(i + (x_half - min_half), i + (y_half - min_half))
-				instance.position = Vector2(pos_shift.x * snap_vec.x, pos_shift.y * snap_vec.y) + Vector2(grid_shift.x, grid_shift.y) - snap_vec / 2
+				instance.position = \
+					Vector2(pos_shift.x * snap_vec.x, pos_shift.y * snap_vec.y) \
+					+ Vector2(grid_shift.x, grid_shift.y) - snap_vec / 2
 				instance.type = type
 				add_child(instance)
 
+# Cancel drag when mouse exists play area
 func confine_to_play_area() -> void:
 	var play_area := grid_size / 2 + Vector2i.ONE
 	var play_area_scaled := Vector2(play_area.x * snap_vec.x, play_area.y * snap_vec.y)
