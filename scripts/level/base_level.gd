f1d4adaac62e858a6e92c617498ac36c4eda0d17
SByte <stormybytes@gmail.com>
2025-11-17 23:30:16 +0700

    feat: export timer duration on base_level node

--- scripts/level/base_level.gd
+++ scripts/level/base_level.gd
@@ -1,10 +1,15 @@
 extends Node
 
 @export var grid_size := Vector2i(4, 4)
+@export var timer_duration: float
 
 @export var play_field: PlayField
+@export var timer: TimerController
 
 func _ready() -> void:
 	play_field.grid_size = grid_size
 	play_field.reset_cats()
+
+	timer.start_duration = timer_duration
+	timer.reset()
 	pass
