1bf6799bfab3135c10b9fbe577eb0fee79ef8bd7
SByte <stormybytes@gmail.com>
2025-11-18 20:22:09 +0700

    fix: skip sentinel types on level generation

--- scripts/level/level_gen.gd
+++ scripts/level/level_gen.gd
@@ -67,10 +67,15 @@ static func gen_cats_rect(grid_size: Vector2i, snap_vec: Vector2, cat_scene: Pac
 	# Instantiate cats
 	for yi in range(grid_size.y):
 		for xi in range(grid_size.x):
+			var type := cat_array[yi * grid_size.x + xi]
+			if type == -1:
+				continue
+
 			var pos := - Vector2(snap_vec.x * grid_size.x / 2, snap_vec.y * grid_size.y / 2) + Vector2(snap_vec.x * xi, snap_vec.y * yi) + Vector2(snap_vec) / 2
+
 			var instance: Cat = cat_scene.instantiate()
 			instance.position = pos
-			instance.set_type(cat_array[yi * grid_size.x + xi])
+			instance.set_type(type)
 			instance.add_to_group("Cats")
 			cat_parent.add_child(instance)
 
