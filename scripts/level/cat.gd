642c9b486cb8b8cbd6b2cbca0ad586d5b50137ce
SByte <stormybytes@gmail.com>
2025-11-19 16:37:09 +0700

    feat: temporary 8 cat variations

--- scripts/level/cat.gd
+++ scripts/level/cat.gd
@@ -12,41 +12,13 @@ const lines : Array[String] = [
 	"Well I'll be damned"
 ]
 
-@export var sprite_res1: CompressedTexture2D
-@export var sprite_res2: CompressedTexture2D
+@export var sprites: Array[CompressedTexture2D]
 
 func set_type(type: int) -> void:
-	match type:
-		1:
-			sprite.texture = sprite_res1
-			sprite.rotation_degrees = 0
-		2:
-			sprite.texture = sprite_res1
-			sprite.rotation_degrees = 90
-		3:
-			sprite.texture = sprite_res1
-			sprite.rotation_degrees = 180
-		4:
-			sprite.texture = sprite_res1
-			sprite.rotation_degrees = 270
-		5:
-			sprite.texture = sprite_res2
-			sprite.rotation_degrees = 0
-		6:
-			sprite.texture = sprite_res2
-			sprite.rotation_degrees = 90
-		7:
-			sprite.texture = sprite_res2
-			sprite.rotation_degrees = 180
-		8:
-			sprite.texture = sprite_res2
-			sprite.rotation_degrees = 270
-		_: pass
+	assert(1 <= type and type <= 8)
+	sprite.texture = sprites[type - 1]
 	self.type = type
 	queue_redraw()
-
-# func set_texture(resource: Texture2D):
-# 	sprite.texture = resource
 
 func _on_static_body_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
