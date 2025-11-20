fbbc52c6c59396a265b83202a50df8287d99da05
TSZCodes <rifqiirfanindrawan@gmail.com>
2025-11-20 18:25:14 +0700

    feat: added blur to pause screen

--- scripts/button/unoverlay.gd
+++ scripts/button/unoverlay.gd
@@ -5,4 +5,3 @@ extends Node
 func _ready() -> void:
 	button.visible = not SceneManager.overlays.is_empty()
 	button.pressed.connect(func() : SceneManager.unoverlay_scene())
-
--- scripts/menus/settings.gd
+++ scripts/menus/settings.gd
@@ -3,10 +3,21 @@ extends Control
 @onready var h_slider: HSlider = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ProgressBar/HSlider
 @onready var progress_bar: ProgressBar = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ProgressBar
 @onready var back_button: Button = $PanelContainer/MarginContainer/HBoxContainer/BackButton
+@onready var main_background: TextureRect = $MainBackground
+@onready var game_background: TextureRect = $GameBackground
 
 var master_bus = AudioServer.get_bus_index("Master")
 
 func _ready() -> void:
+	var pause_menu = get_tree().get_first_node_in_group("PauseMenu")
+	print(SceneManager.path_history.get(0), SceneManager.overlays.get(0))
+	if not SceneManager.path_history.is_empty() or not SceneManager.overlays.is_empty():
+		if SceneManager.path_history.get(0) == "res://scenes/menus/main_menu.tscn" && SceneManager.overlays.get(0) != pause_menu: 
+			main_background.visible = true
+			game_background.visible = false
+		elif SceneManager.overlays.get(0) == pause_menu:
+			main_background.visible = false
+			game_background.visible = true
 	progress_bar.value = h_slider.value
 
 func _on_h_slider_value_changed(value: float) -> void:
