16c205376c731fddc0cf114e55d0167f09f46a89
SByte <stormybytes@gmail.com>
2025-11-22 20:29:03 +0700

    feat: bingus dialog sfx

--- scripts/cutscene/character_resource.gd
+++ scripts/cutscene/character_resource.gd
@@ -3,3 +3,5 @@ extends Resource
 
 @export var name: String
 @export var sprites: Dictionary[String, CompressedTexture2D]
+
+@export var dialog_sfx: AudioStream
--- scripts/cutscene/scene.gd
+++ scripts/cutscene/scene.gd
@@ -29,11 +29,13 @@ func _ready() -> void:
 	exec_entry.call_deferred()
 
 func advance_entry() -> void:
+	# Forcefully finish the text animation
 	if text_idx + 1 < text.length():
 		text_timer.stop()
 		dialog_text.text = text
 		text_idx = text.length()
 		comps_finished += 1
+		on_text_animate_finished()
 		return
 
 	if comps_finished < entries[entry_idx].components.size():
@@ -69,6 +71,7 @@ func exec_entry() -> void:
 			text_idx = 0
 			if not text.is_empty():
 				next_letter()
+				on_text_animate_started(comp.character.dialog_sfx)
 
 		elif component is CutsceneCharEnter:
 			var comp = (component as CutsceneCharEnter)
@@ -132,6 +135,7 @@ func next_letter() -> void:
 
 	if text_idx + 1 >= text.length():
 		comps_finished += 1
+		on_text_animate_finished()
 		return
 
 	text_idx += 1
@@ -139,3 +143,11 @@ func next_letter() -> void:
 
 func on_text_timer_finished() -> void:
 	next_letter()
+
+func on_text_animate_started(stream: AudioStream) -> void:
+	var player := AudioManager.reusable_player
+	player.stream = stream
+	player.play()
+
+func on_text_animate_finished() -> void:
+	AudioManager.reusable_player.stop()
