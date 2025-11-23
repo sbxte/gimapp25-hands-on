c46782967e87d8227985791c883d2e9bba26938f
SByte <stormybytes@gmail.com>
2025-11-23 17:04:12 +0700

    fix: next level button

---
+++ scripts/button/next_level.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export var play_field: PlayField
+@export_file("*.tscn") var level_paths: Array[String] = ["", "", "", "", ""]
+@export_file("*.tscn") var level_menu: String
+@onready var button: Button = $".."
+
+
+func _ready() -> void:
+	button.pressed.connect(func():
+		var level := play_field.level - 1
+		if level < level_paths.size():
+			SceneManager.change_scene(level_paths[level], false)
+		else:
+			SceneManager.change_scene(level_menu, false)
+	)
