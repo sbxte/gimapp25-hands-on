296666afe7036f174c81b33762d38720a931b444
SByte <stormybytes@gmail.com>
2025-11-25 08:42:03 +0700

    style: comments on signals

--- scripts/events.gd
+++ scripts/events.gd
@@ -1,10 +1,15 @@
 extends Node
 
+# Matching mechanics
 signal cat_mouse_click(pos: Vector2, cat: Cat)
 signal cat_mouse_enter(pos: Vector2, cat: Cat)
 signal cancel_drag
-signal finished_displaying()
-signal advance_cutscene()
+
+# Speech bubbles
+signal finished_displaying
+
+# Cutscene handling
+signal advance_cutscene
 signal cutscene_visibility_changed(visible: bool)
 signal cutscene_dialog_box_enabled(enabled: bool)
 signal cutscene_custom_comp(cutscene_node: Node, comp: CutsceneComponent)
