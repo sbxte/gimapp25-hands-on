221e3f88816ecdf23ef28cb55f9037fac72f4dd6
SByte <stormybytes@gmail.com>
2025-11-25 14:29:57 +0700

    fix: dev_util reset data and reset cats encountered

--- scripts/dev_util.gd
+++ scripts/dev_util.gd
@@ -7,15 +7,18 @@ func enabled() -> bool:
 	return OS.has_feature("debug")
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if test_keybind(event, reset_data_keybind):
-		print("Save Data Reset!")
-	if test_keybind(event, reset_cats_encountered_keybind):
-		print("Cats Encountered Reset!")
-
-func test_keybind(event: InputEvent, keybind: Array[String]) -> bool:
-	if keybind.has(OS.get_keycode_string((event as InputEventKey).keycode)) \
-		and keybind.all(func(label: String): return Input.is_key_pressed(OS.find_keycode_from_string(label))):
+	test_keybind(event, reset_data_keybind, func():
 		SaveSystem.reset_data()
 		SaveSystem.write_data()
-		return true
-	return false
+		print("Save Data Reset!")
+	)
+	test_keybind(event, reset_cats_encountered_keybind, func():
+		SaveSystem.get_data().cats_encountered.fill(false)
+		SaveSystem.write_data()
+		print("Cats Encountered Reset!")
+	)
+
+func test_keybind(event: InputEvent, keybind: Array[String], callback: Callable):
+	if keybind.has(OS.get_keycode_string((event as InputEventKey).keycode)) \
+		and keybind.all(func(label: String): return Input.is_key_pressed(OS.find_keycode_from_string(label))):
+		callback.call()
