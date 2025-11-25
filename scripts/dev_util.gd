0506f56ed677e63bf3076815d116a74bc636f359
SByte <stormybytes@gmail.com>
2025-11-25 08:21:30 +0700

    feat: cats_encountered reset shortcut via dev_utils

--- scripts/dev_util.gd
+++ scripts/dev_util.gd
@@ -1,13 +1,21 @@
 extends Node
 
-var reset_data_shortcut := ["8", "2"]
+var reset_data_keybind: Array[String] = ["8", "2"]
+var reset_cats_encountered_keybind: Array[String] = ["8", "0"]
 
 func enabled() -> bool:
 	return OS.has_feature("debug")
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if reset_data_shortcut.has(OS.get_keycode_string((event as InputEventKey).keycode)):
-		if reset_data_shortcut.all(func(label: String): return Input.is_key_pressed(OS.find_keycode_from_string(label))):
-			SaveSystem.reset_data()
-			SaveSystem.write_data()
-			print("Save Data Reset!")
+	if test_keybind(event, reset_data_keybind):
+		print("Save Data Reset!")
+	if test_keybind(event, reset_cats_encountered_keybind):
+		print("Cats Encountered Reset!")
+
+func test_keybind(event: InputEvent, keybind: Array[String]) -> bool:
+	if keybind.has(OS.get_keycode_string((event as InputEventKey).keycode)) \
+		and keybind.all(func(label: String): return Input.is_key_pressed(OS.find_keycode_from_string(label))):
+		SaveSystem.reset_data()
+		SaveSystem.write_data()
+		return true
+	return false
