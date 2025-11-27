a932d6e384fa4c4da6ee474d673dc7d1b4b179a4
SByte <stormybytes@gmail.com>
2025-11-27 16:05:12 +0700

    fix: only cancel level advance when entering epilogue

--- scripts/button/enter_epilogue_behavior.gd
+++ scripts/button/enter_epilogue_behavior.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	Events.pre_advance_level.connect(on_pressed)
 
 func on_pressed() -> void:
-	Events.cancel_advance_level.emit()
 	if not SaveSystem.get_data().has_seen_epilogue:
+		Events.cancel_advance_level.emit()
 		SceneManager.change_scene(epilogue_scene, false)
 
