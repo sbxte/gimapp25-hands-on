70f569b7ee72054e8aeab0690deb35e0aba0d85c
SByte <stormybytes@gmail.com>
2025-11-27 21:51:14 +0700

    fix: 430a98e bruh

--- scripts/button/quit.gd
+++ scripts/button/quit.gd
@@ -3,6 +3,8 @@ extends Node
 @onready var button: BaseButton = $".."
 
 func _ready() -> void:
-	AudioManager.stop_all_music()
-	button.pressed.connect(func() : get_tree().quit())
+	button.pressed.connect(func() :
+		AudioManager.stop_all_music()
+		get_tree().quit()
+	)
 
